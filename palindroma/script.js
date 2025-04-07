78c1c9fb866002a5992478e2979c061eaabe342f
arielmusa <arielmusainfo@gmail.com>
2025-04-07 15:30:24 +0200

    first commit

---
+++ palindroma/script.js
@@ -0,0 +1,19 @@
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+const userWord = prompt("Inserisci una parola");
+
+// utilizzo il ciclo for per prendere una lettera alla volta e le aggiungo a una nuova variabile
+// dopo verifico se la nuova variabile equivale alla variabile iniziale
+function isPalindroma(word) {
+  let reverseWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    currentLetter = word[i];
+    reverseWord += currentLetter;
+    console.log(reverseWord);
+  }
+  if (reverseWord === userWord) return true;
+  else return false;
+}
+
+console.log(isPalindroma(userWord));
